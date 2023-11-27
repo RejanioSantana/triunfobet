2823841b501af6a89086f2ab28f0651b48552683
Rejanio Santos <jhunior.gt@gmail.com>
2023-11-27 15:16:56 -0300

    Refatoração AJAX e Dependecias.

--- assets/js/dependencias-section-jogos.js
+++ assets/js/dependencias-section-jogos.js
@@ -1,40 +1,12 @@
 // Dps alterar para querySelectorAll.
 var btn_sction_bolao = document.querySelector('.btn-area-bolao');
 btn_sction_bolao.addEventListener('click',
-() =>fetchSectionBolao(btn_sction_bolao));
-
-async function fetchSectionBolao(el){
-    try{
-        const href = el.getAttribute('href');
-        const request = await  fetch(href);
-        if(request.status !== 200) throw new Error('ERRO 404 meu');
-        const html = await request.text();
-        carregarAreaBolao(html);
-
-    } catch(e){ console.log(e);}
-
-}
-function carregarAreaBolao(response){
-
-    // Original
-    const mainCentral = document.querySelector("#main-central");
-    // console.log(response);
-    mainCentral.innerHTML = response;
-
-// ****************************
-    let script = document.querySelector(
-        'script[src="assets/js/dependencias-section-jogos.js"]');
-
-    // Verifique se o elemento existe antes de tentar removê-lo
-    if(script){
-        let parentElement = script.parentNode;
-        // Remova o elemento <script> do pai
-        parentElement.removeChild(script);
-    }
-
+() =>sectionBolao(btn_sction_bolao));
+async function sectionBolao(e){
+    await carregarSection(e);
+    reniciandoDependencias();
     script = document.createElement('script');
     script.src = 'assets/js/bolao.js';
     script.async = true;
     document.body.appendChild(script);
-
 }
--- assets/js/index.js
+++ assets/js/index.js
@@ -7,38 +7,32 @@ const fade_menu = document.querySelector(".fade-menu");
 const modal_login = document.querySelector(".modal-login");
 const btn_menu_modal = document.querySelector(".btn-menu-modal");
 const modal_menu = document.querySelector(".modal-menu");
-
 const toggleModalFade = () => fade.classList.toggle("hidden");
 const toggleModalFadeMenu = () => fade.classList.toggle("hidden");
 const toggleModalLogin = () =>modal_login.classList.toggle("hidden");
 const toggleModalMenu = () => modal_menu.classList.toggle("hidden");
-
 btn_login_header.addEventListener("click",()=> {
     toggleModalLogin();
     toggleModalFade()});
 btn_menu_modal.addEventListener("click",() => {
     toggleModalMenu();
     toggleModalFadeMenu()});
-
 const btn_section_jg = document.querySelector(".btn-section-jogos");
 const btn_section_apostas = document.querySelector(".btn-section-apostas");
-
 const toggleSection = () =>{
     btn_section_jg.classList.toggle("action");
     btn_section_apostas.classList.toggle("action");
 }
-
 // carregando section e dependencias automaticamente
 if(btn_section_jg.classList.contains("action")){
-    addDependenciasSectionJogos(btn_section_jg);
+    addSectionJogos(btn_section_jg);
 }
-
 // event click section principal.
 [btn_section_apostas,btn_section_jg].forEach( (e)=>{
     e.addEventListener("click",()=>{
         e.target;
         if(e.classList.contains('btn-section-jogos')){
-            addDependenciasSectionJogos(e)
+            addSectionJogos(e)
             if(e.classList.contains("action")){
                 return
             }
@@ -46,18 +40,24 @@ if(btn_section_jg.classList.contains("action")){
         }
     });
 });
-
-async function addDependenciasSectionJogos(e){
-    await carregarSectionJogos(e);
+async function addSectionJogos(e){
+    await carregarSection(e);
+    reniciandoDependencias();
+    script = document.createElement('script');
+    script.src = 'assets/js/dependencias-section-jogos.js';
+    script.async = true;
+    document.body.appendChild(script);
 }
-function reniciandoDependencias(){
-    var scripts = document.getElementsByTagName('script');
-    scripts.forEach(e =>{
-        console.log(e)
-    })
+async function addDependenciasSectionJogos(e){
+    await carregarSection(e);
+    reniciandoDependencias();
+    script = document.createElement('script');
+    script.src = 'assets/js/dependencias-section-jogos.js';
+    script.async = true;
+    document.body.appendChild(script);
 }
 // Ajax
-async function carregarSectionJogos(el){
+async function carregarSection(el){
     try{
         const href = el.getAttribute('href');
         const request = await  fetch(href);
@@ -68,24 +68,11 @@ async function carregarSectionJogos(el){
 }
 function carregarResultado(response){
     const mainCentral = document.querySelector("#main-central");
-    mainCentral.innerHTML = response;
-    reniciandoDependencias();
-    let script = document.querySelector(
-    'script[src="assets/js/dependencias-section-jogos.js"]');
-    if(script){
-        let parentElement = script.parentNode;
-        // Remova o elemento <script> do pai
-        parentElement.removeChild(script);
-    }
-
-    script = document.createElement('script');
-    script.src = 'assets/js/dependencias-section-jogos.js';
-    script.async = true;
-    document.body.appendChild(script);
+    mainCentral.innerHTML = response; 
 }
 // Function deletar scripts desnecessarios!
 function reniciandoDependencias(){
-    var scripts = document.querySelectorAll('script[src="assets/js/bolao.js"]');
+    var scripts = document.querySelectorAll('script');
     scripts.forEach(e =>{
         let parentElement = e.parentNode;
         // Remova o elemento <script> do pai
